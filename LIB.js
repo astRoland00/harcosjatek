c7939cb54c78cde3bf4d24a7566a66512ee7c2d2
astRoland00 <petenyisajat@gmail.com>
2025-06-18 22:40:46 +0300

    .

---
+++ LIB.js
@@ -0,0 +1,102 @@
+export function wait(num) {
+    return new Promise(resolve => setTimeout(resolve, num));
+}
+export function waitUntil(conditionFunction, interval = 100) {
+    return new Promise(resolve => {
+      const checkCondition = () => {
+        if (conditionFunction()) {
+          resolve();
+        } else {
+          setTimeout(checkCondition, interval);
+        }
+      };
+      checkCondition();
+    });
+  }
+export class Player{
+    
+    constructor(name, health, pos,direction, animlist)
+    {
+        this.Name = name
+        this.Pos = pos
+        this.Health = health
+        this.Direction = direction
+        this.AnimList = animlist
+        this.Status = 0
+        this.CurrentAnim = this.AnimList[this.Status]
+    }
+    async SetAnim(){
+        if (!this.CurrentAnim.Imp && (this.AnimList[this.Status].Imp || !this.AnimList[this.Status].Imp)) {
+            this.CurrentAnim = this.AnimList[this.Status]
+            this.SetAnim()
+        }else{
+            await waitUntil(()=> this.CurrentAnim.CurrentFrame == this.CurrentAnim.Frames[this.CurrentAnim.Frames.length-1])
+            this.CurrentAnim = this.AnimList[this.Status]
+            await wait((1000/10)*4)
+            this.Status = 0
+            this.SetAnim()
+
+        }
+    }
+    async AnimationPlayer(){
+        for (let i = 0; i < this.CurrentAnim.Frames.length; i++) {
+            this.CurrentAnim.CurrentFrame = this.CurrentAnim.Frames[i]
+            await wait(1000/10)
+        }
+        this.AnimationPlayer()
+    }
+    async Jump(){
+        this.Status = 0
+        this.SetAnim()
+        for (let i = 0; i > -1; i-=0.1) {
+            this.Pos.y += i
+            await wait(40)
+        }
+        await wait(100)
+        for (let i = 0; i < 1; i+=0.1) {
+            this.Pos.y += i
+            await wait(20)
+        }
+        
+    }
+    async Move(num){
+        this.Status = 1
+        this.SetAnim()
+        this.Direction = num
+        for (let i = 0; i < 20; i++) {
+            this.Pos.x += num/10
+            await wait(20)
+        }
+        
+    }
+    Desh(){
+        this.Status = 2
+        this.SetAnim()
+        this.Pos.x += this.Direction*8
+    }
+}
+export class Animation{
+    constructor(name,length,imp)
+    {
+        this.Name = name
+        this.Frames = []
+        this.CurrentFrame
+        this.Imp = imp
+        for (let i = 0; i < length; i++) {
+            this.Frames.push(`${name}/${name}${i}.png`)
+        }
+    }
+}
+
+export class Collider{
+    constructor(width,heigth){
+        this.ColliderData
+    }   
+}
+
+export class Pos {
+    constructor(x,y) {
+        this.x = x
+        this.y = y
+    }
+}
---
+++ main.js
@@ -0,0 +1,62 @@
+import { Player, Pos ,Animation, wait} from "./LIB.js";
+
+let player = new Player("player",100,new Pos(20,16),1,[new Animation("idle",10,false),new Animation("run",16,false),new Animation("dash",4,true),new Animation("attack",7,true),new Animation("hurt",4,true)])
+let playerelement = document.getElementById("player")
+async function Game() {
+    playerelement.style.backgroundImage = `url(${player.CurrentAnim.CurrentFrame})`
+    playerelement.style.left = `${player.Pos.x * 32}px`
+    playerelement.style.top = `${player.Pos.y * 32}px`
+    playerelement.style.translate = `-50% -50%`
+    await wait(50)
+    Game()
+}
+
+addEventListener("keyup", (event) => {
+    if (event.key === "a" || event.key === "d") {
+        player.Status = 0
+        player.SetAnim()
+    }
+})
+addEventListener("keyup", (event) => {
+    if (event.key == "w") {
+        if (player.Pos.y == 16) {
+            player.Jump()
+        }
+    }
+})
+
+addEventListener("keydown", (event) => { 
+    switch (event.key) {
+        
+
+        case "a": 
+            if (player.Pos.x*32 > (window.screen.width/playerelement.getBoundingClientRect().width+playerelement.getBoundingClientRect().width/2)){
+                player.Move(-1)
+                playerelement.style.transform = `scaleX(${player.Direction})`
+            }      
+            
+
+            break;
+        case "d":
+            if (player.Pos.x*32 < (window.screen.width-playerelement.getBoundingClientRect().width/1.1)) {
+                player.Move(1)
+                playerelement.style.transform = `scaleX(${player.Direction})`
+            }
+            
+            break;
+        case "e":
+            if (player.Pos.x*32 < (window.screen.width-playerelement.getBoundingClientRect().width/1.1)&&player.Pos.x*32 > (window.screen.width/playerelement.getBoundingClientRect().width+playerelement.getBoundingClientRect().width/2)) {
+                player.Desh()
+            }
+            break
+
+    }
+})
+
+window.onload = function(){
+    window.player = player
+    let rand = ["gif"]
+    document.body.style.backgroundImage = `url(bg.${rand[Math.floor(Math.random()*rand.length)]})`
+    player.AnimationPlayer()
+    Game()
+}
